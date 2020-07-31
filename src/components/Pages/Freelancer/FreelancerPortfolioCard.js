cb22dbdadf456b4a72939b369a6a9f3ca0182025
petar-staynov <petar.staynov@gmail.com>
2020-07-31 12:42:45 +0300

    Portfolio Card Improvements
    - Added title to card
    - Made cards bigger
    - Rounded image corners

--- src/components/Pages/Freelancer/FreelancerPortfolioCard.js
+++ src/components/Pages/Freelancer/FreelancerPortfolioCard.js
@@ -4,7 +4,7 @@ import FreelancerSectionHeading from "./FreelancerSectionHeading";
 const FreelancerPortfolioCard = ({data}) => {
     const {id, imgUrl, text, title} = data;
 
-    const [modalData, setModalData] = useState({});
+    const [modalData, setModalData] = useState(null);
 
     const handleOnClick = () => {
         modalData === null
@@ -48,18 +48,9 @@ const FreelancerPortfolioCard = ({data}) => {
 
             <div className="col-md-6 col-lg-4 mb-5 hovercard" onClick={handleOnClick}>
                 <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-                    <div
-                        className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                        <div className="portfolio-item-caption-content text-center text-white">
-                            <svg className="svg-inline--fa fa-plus fa-w-14 fa-3x" aria-hidden="true"
-                                 focusable="false" data-prefix="fas" data-icon="plus" role="img"
-                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                                <path fill="currentColor"
-                                      d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z">
-                                </path>
-                            </svg>
-                        </div>
+                    <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <img className="img-fluid" src={`./assets/${imgUrl}`} alt=""/>
+                        <p className={null}>{title}</p>
                     </div>
                 </div>
             </div>
